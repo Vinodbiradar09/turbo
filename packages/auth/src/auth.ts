76aec90c4c57e6e63e79be2d7cb4b237321a5b35
Vinodbiradar09 <vinodjb07@gmail.com>
2026-02-16 20:28:51 +0530

    betterauth

---
+++ packages/auth/src/auth.ts
@@ -0,0 +1,20 @@
+import { prisma } from "@repo/db";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from 'better-auth/adapters/prisma'
+
+export const auth = betterAuth({
+    baseURL : process.env.BETTER_AUTH_URL,
+    database : prismaAdapter(prisma , {
+        provider : "postgresql",
+    }),
+   socialProviders : {
+    google : {
+        clientId : process.env.GOOGLE_CLIENT_ID!,
+        clientSecret : process.env.GOOGLE_CLIENT_SECRET!,
+    },
+    github : {
+        clientId: process.env.GITHUB_CLIENT_ID!,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }
+   }
+})
